503685893846053243fc959a27d6d9fddd55a297
LuzEsperanza <luzdeesperanzaapolo@gmail.com>
2020-12-18 22:27:09 -0300

    head

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -5,6 +5,7 @@ import {createStackNavigator} from '@react-navigation/stack'
 import ListProjects from './pages/LiatProjects'
 import ProjectsRegister from './pages/ProjectsRegister'
 import ProductsDetales from './pages/ProjectsDetalhes';
+import Header from  './components/Header';
 
 // import { Container } from './styles';
 const {Navigator, Screen} = createStackNavigator();
@@ -12,10 +13,20 @@ const Routes: React.FC = () => {
   return (
     <NavigationContainer>
       
-        <Navigator>
-          <Screen name='ListProjects' component={ListProjects}/>
-          <Screen name='ProjectsDetales' component={ProductsDetales}/>
-          <Screen name='ProjectsRegister' component={ProjectsRegister}/>
+        <Navigator screenOptions={{headerShown:false}}>
+          <Screen 
+            name='ListProjects' component={ListProjects}  options={{
+            headerShown:true, 
+            header:()=><Header titulo='Lista Projetos'/>}}
+          />
+          <Screen
+           name='ProjectsDetales' component={ProductsDetales}  options={{
+            headerShown:true, 
+            header:()=><Header titulo='Detalhes Projetos'/>}}
+           />
+          <Screen name='ProjectsRegister' component={ProjectsRegister}  options={{
+            headerShown:true, 
+            header:()=><Header titulo='Tela de Cadastro'/>}}/>
         </Navigator>
         
        
